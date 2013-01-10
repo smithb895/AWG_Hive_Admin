646dc1ce8e2ab86c59770d59200e73bb2a6bbd6f
Bob Smith <smithb895@gmail.com>
2013-01-10 23:07:16 +0800

    Put DB connection in Try-Catch block
    To prevent DB connection info from being displayed upon errors.

--- hive_connect.php
+++ hive_connect.php
@@ -3,7 +3,11 @@ require("session.php");
 require("config.php");
 
 //function hiveconnect($hive_address,$hive_db,$hive_user,$hive_pass) {
-$dbhandle = new PDO("mysql:host=$hive_address;dbname=$hive_db", $hive_user, $hive_pass);
+try {
+	$dbhandle = new PDO("mysql:host=$hive_address;dbname=$hive_db", $hive_user, $hive_pass);
+} catch (PDOException $err) {
+	die($err->getMessage());
+}
 //return $dbhandle;
 //}
 
