313dc5becbbec33903aa6784d81bde0aa638a345
Bob Smith <smithb895@gmail.com>
2013-01-15 19:10:49 +0800

    Added SQL Setup Script
    Sets up tables needed for `hive_admin` database.

---
+++ _setup/AWG_Hive_Admin_SQL_Setup.sql
@@ -0,0 +1,90 @@
+-- MySQL dump 10.13  Distrib 5.5.27, for Win64 (x86)
+--
+-- Host: localhost    Database: hive_admin
+-- ------------------------------------------------------
+-- Server version	5.5.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `blocked_ips`
+--
+
+DROP TABLE IF EXISTS `blocked_ips`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blocked_ips` (
+  `ip` varchar(15) NOT NULL,
+  `date_added` timestamp NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ip`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `hive_admins`
+--
+
+DROP TABLE IF EXISTS `hive_admins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hive_admins` (
+  `id` int(2) unsigned NOT NULL AUTO_INCREMENT,
+  `hive_user` varchar(64) NOT NULL,
+  `hive_password` varchar(512) NOT NULL,
+  `salt` varchar(64) NOT NULL,
+  `salt2` varchar(64) NOT NULL,
+  `tier` smallint(2) unsigned NOT NULL DEFAULT '2',
+  `lastlogin` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `locked` smallint(1) unsigned NOT NULL DEFAULT '0',
+  `failed_attempts` smallint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hive_admins`
+--
+-- Default username: admin
+-- Default password: TopSecretPassword123
+LOCK TABLES `hive_admins` WRITE;
+/*!40000 ALTER TABLE `hive_admins` DISABLE KEYS */;
+INSERT INTO `hive_admins` VALUES (1,'admin','eeda72e531460344de26a5a75942e9a3c5eb03f1ad020b616a7abb21228221de6078ad5f7d5a5dc6786e16f83352ff2d40046cbe97d568eebeb27f1511dfad3a','g81lndu0w9_medwo7qy3pn7dj5j+3ywacls_lw04ll4rs9fsy,p-a_*r_d,l3pox','qtr6,16,srtqe+eqx+xqc5+zwhq-jcw8bv$co8hk7srjhpvj6+c8u0i*=fw99,cj',1,NULL,0,0);
+/*!40000 ALTER TABLE `hive_admins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `logs`
+--
+
+DROP TABLE IF EXISTS `logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `logs` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `action` varchar(255) DEFAULT NULL,
+  `user` varchar(255) NOT NULL DEFAULT '',
+  `timestamp` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1934 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-01-15 18:58:27
